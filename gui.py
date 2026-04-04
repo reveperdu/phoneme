5ef6803e53053318bd99653092965b7c329c2aa5
wbw <109431485+reveperdu@users.noreply.github.com>
2026-04-04 18:05:34 +0900

    rewrite qt imports

--- gui.py
+++ gui.py
@@ -1,24 +1,25 @@
 import re
-import PySide6.QtWidgets as qt
+from PySide6.QtWidgets import (QApplication,QWidget,QPlainTextEdit,QComboBox,
+                               QGridLayout,QPushButton)
 from PySide6.QtGui import QTextCursor
 from PySide6.QtCore import QTimer
-import api
+from api import *
 subs_dict={"full-width quotes to half-width":{r'“|”':r'"'},
            "half-width quotes to full-width":{r'\"(.*?)\"',r'“\1”'}}
 #ui layout
-qt_app=qt.QApplication()
-qt_window=qt.QWidget()
-qt_text_main=qt.QPlainTextEdit()
-qt_combo=qt.QComboBox()
-qt_layout=qt.QGridLayout()
-qt_button_subs=qt.QPushButton("Do It")
-qt_button_send=qt.QPushButton("Send")
-qt_window.setLayout(qt_layout)
-for w in [qt_text_main,qt_combo,qt_button_subs,qt_button_send]:
-    qt_layout.addWidget(w)
-qt_combo.addItems(list(subs_dict.keys()))
-qt_window.show()
-qt_tstream=QTimer()
+app=QApplication()
+window=QWidget()
+tmain=QPlainTextEdit()
+combo=QComboBox()
+layout=QGridLayout()
+bsubs=QPushButton("Do It")
+bsend=QPushButton("Send")
+window.setLayout(layout)
+for w in [tmain,combo,bsubs,bsend]:
+    layout.addWidget(w)
+combo.addItems(list(subs_dict.keys()))
+window.show()
+tstream=QTimer()
 '''stream update timer'''
 # logic
 def text_subs(text,param):
@@ -28,27 +29,27 @@ def text_subs(text,param):
     return text
 
 def text_cb():
-    param=qt_combo.currentText()
-    newtext=text_subs(qt_text_main.toPlainText(),param)
-    qt_text_main.setPlainText(newtext)
+    param=combo.currentText()
+    newtext=text_subs(tmain.toPlainText(),param)
+    tmain.setPlainText(newtext)
 def send():
     global current_stream
-    context=qt_text_main.toPlainText()
-    current_stream=api.textcomp_stream(context)
-    qt_tstream.start()
+    context=tmain.toPlainText()
+    current_stream=textcomp_stream(context)
+    tstream.start()
 def stream_tick():
     chunk=next(current_stream,None)
     if chunk is not None:
         #"append" gives extra newline
-        qt_text_main.moveCursor(QTextCursor.MoveOperation.End)
-        qt_text_main.insertPlainText(chunk)
+        tmain.moveCursor(QTextCursor.MoveOperation.End)
+        tmain.insertPlainText(chunk)
     else:
-        qt_tstream.stop()
+        tstream.stop()
 # ui events
-qt_button_subs.clicked.connect(text_cb)
-qt_button_send.clicked.connect(send)
-qt_tstream.setInterval(100)
-qt_tstream.timeout.connect(stream_tick)
+bsubs.clicked.connect(text_cb)
+bsend.clicked.connect(send)
+tstream.setInterval(100)
+tstream.timeout.connect(stream_tick)
 # exec
-qt_app.exec()
+app.exec()
     
