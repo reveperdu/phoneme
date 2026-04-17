07cd9ed1dfc4b7561d98eef4f9470505b24af4f9
wbw <109431485+reveperdu@users.noreply.github.com>
2026-04-17 21:19:32 +0900

    basic agent functionality

--- handler.py
+++ handler.py
@@ -1,7 +1,8 @@
 from typing import TYPE_CHECKING
+import re
 from PySide6.QtGui import QTextCursor
 from api import textcomp_stream, abort
-
+from utils import make_subsdict,tool_call_generic
 if TYPE_CHECKING:
     from window import Window
 
@@ -14,6 +15,7 @@ class Handler:
         self.config = self.w.config
 
     def send(self):
+        self.current_output=""
         context = self.w.maintext.toPlainText()
         self.last_context = context
         inputmsg = self.w.inputtext.text()
@@ -21,11 +23,11 @@ class Handler:
         if is_new_turn:
             context = context + "\n{{[INPUT]}}\n" + inputmsg + "\n{{[OUTPUT]}}\n"
             self.last_context = context
-        d = self.config["chat_template"]
+        d = make_subsdict(self.config)
         prom = context
         if self.no_think:
             s1, s2, s3 = prom.rpartition("\n{{[OUTPUT]}}\n")
-            prom = s1 + s2 + self.config["nothink_tag"] + s3
+            prom = s1 + s2 + self.config["chat_template"]["nothink"] + s3
         for k in d:
             prom = prom.replace(k, d[k])
         self.w.inputtext.clear()
@@ -49,8 +51,22 @@ class Handler:
             # "append" gives extra newline
             self.w.maintext.moveCursor(QTextCursor.MoveOperation.End)
             self.w.maintext.insertPlainText(chunk)
+            self.current_output+=chunk
         else:
             self.w.tstream.stop()
+            self.output_postprocess()
 
     def abort(self):
         abort(self.config["api_abort"])
+    def output_postprocess(self):
+        t=self.config["chat_template"]
+        pattern=t["tool_call_start"]+"(.*)"+t["tool_call_end"]
+        tool_match=re.search(pattern,self.current_output)
+        if tool_match:
+            call=tool_match[1]
+            result=tool_call_generic(call)
+            #note two \n help suppress possible generation of tool response tag
+            result=f"\n\n{t["tool_resp_start"]}{result}{t["tool_resp_end"]}\n"
+            self.w.maintext.insertPlainText(result)
+            #continue generation after tool returns
+            self.send()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from PySide6.QtWidgets import QApplication
 from window import Window
 
+TEST = 0
 subs_dict = {
     "full-width quotes to half-width": {r"“|”": r'"'},
     "half-width quotes to full-width": {r"\"(.*?)\"", r"“\1”"},
@@ -16,6 +17,10 @@ win = Window(config=config)
 win.maintext.setPlainText(config["default_prompt"])
 win.show()
 # set dark mode
-stylestr = "*{background:#303030;color:white;font-size:16pt}"
+stylestr = "*{background:#303030;color:white}"
 app.setStyleSheet(stylestr)
+if TEST:
+    testmsg = "你好呀"
+    win.inputtext.insert(testmsg)
+    win.send.click()
 app.exec()
