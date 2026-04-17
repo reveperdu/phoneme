b044439cf4f6487cc8d042cb319e33a2a2d5248f
reveperdu <109431485+reveperdu@users.noreply.github.com>
2026-04-17 20:53:32 +0900

    Clean up comments in main.py
    Removed commented-out sections related to UI layout and events.

--- main.py
+++ main.py
@@ -6,14 +6,10 @@ subs_dict = {
     "full-width quotes to half-width": {r"“|”": r'"'},
     "half-width quotes to full-width": {r"\"(.*?)\"", r"“\1”"},
 }
-# ui layout
 app = QApplication()
-# stream timer
 
 with open("config.json") as f:
     config = json.load(f)
-
-# ui events
 
 # setup ui & app
 win = Window(config=config)
