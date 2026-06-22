8a068006be36543063f181d11806329ea16891bd
wbw <109431485+reveperdu@users.noreply.github.com>
2026-06-22 21:43:45 +0900

    changed math renderer ui

--- rendermath.py
+++ rendermath.py
@@ -6,11 +6,14 @@ html_template = r"""
     <link rel="stylesheet" href="javascript/katex/katex.min.css">
     <script src="javascript/katex/katex.min.js"></script>
 </head>
+
 <body>
-    <p id="content">[PLACEHOLDER]</p>
+    <textarea id="content" style="width:30em;height:3em">[PLACEHOLDER]</textarea><br>
+    <button onclick="rendermath()">render</button>
+    <p id="output"></p>
     <!--if script is placed in head, it will be unable to find element-->
     <script>
-        function rendermath(text) {
+        function rendermathStr(text) {
             return text.replace(/\$\$([\s\S]*?)\$\$|\$([\s\S]*?)\$/g, (full, block, inline) => {
                 if (block) {
                     return katex.renderToString(block, { displayMode: true, throwOnError: false });
@@ -20,15 +23,23 @@ html_template = r"""
                 }
             })
         }
-        e=document.getElementById("content")
-        e.innerHTML=rendermath(e.innerHTML)
+        function rendermath() {
+            eContent = document.getElementById("content")
+            eOutput = document.getElementById("output")
+            eOutput.innerHTML = rendermathStr(eContent.value)
+        }
+
+        rendermath()
     </script>
 </body>
+
 </html>
 """
 
+default_math = r"default: $$\frac{\partial u}{\partial t}+u\frac{\partial u}{\partial x}=\frac{\partial^2 u}{\partial x^2}$$"
 
-def render_math(text: str):
+
+def render_math(text: str = default_math):
     from PySide6.QtWebEngineWidgets import QWebEngineView
     from PySide6.QtCore import QUrl
 
@@ -41,3 +52,11 @@ def render_math(text: str):
     # without some tweaking webview seems to be unable to access local files
     view.setHtml(html, QUrl.fromLocalFile("/usr/share/"))
     view.show()
+
+
+if __name__ == "__main__":
+    from PySide6.QtWidgets import QApplication
+
+    app = QApplication()
+    render_math(default_math)
+    app.exec()
